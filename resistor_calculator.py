6dc657b7ae8a6358d610fca5f4c13324bf8ea447
Beedlebub <beedlebub@gmail.com>
2020-10-10 07:44:17 -0700

    .

--- resistor_calculator.py
+++ resistor_calculator.py
@@ -6,8 +6,8 @@ calulation_type = input('Enter "s" for series or "p" for parallel calulation: ')
 if calulation_type == "s":
     series_result = int(resistor_one) + int(resistor_two)
     print('The value in series is:', series_result, '\n')
-if calculation_type == "p":
-    parallel_result = int(resistor_one/1) + int(resistor_two/1)
-    print(('The value in parallel is:', parallel_result, '\n')
-else if:
-    print('Thanks, Brand!')
+if calulation_type == "p":
+    parallel_result = int(resistor_one) + int(resistor_two)
+    print('\nThe value in parallel is:', parallel_result, '\n')
+else:
+    print('Thanks, Brand!\n')
