bad7c73affbda47dffca10f060e856d619c40840
Beedlebub <beedlebub@gmail.com>
2021-02-06 10:36:25 -0800

    .

--- random_twoDieRoll.py
+++ random_twoDieRoll.py
@@ -9,10 +9,10 @@ for x in range(1, 11):
     total = die_1 + die_2
     print(total)
     if total == 7:
-        print('Seven Thrown!\t', die_1, die_2, '\n')
+        print('Seven Thrown!\t', die_1, 'and', die_2, '\n')
     if total == 11:
-        print('Eleven Thrown!\t', die_1, die_2, '\n')
+        print('Eleven Thrown!\t', die_1, 'and', die_2, '\n')
     if die_1 == die_2:
-        print('Doubles Thrown!\t', die_1, die_2, '\n')
+        print('Doubles Thrown!\t', die_1, 'and', die_2, '\n')
 
 print('')       
